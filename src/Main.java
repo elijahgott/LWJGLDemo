b5a4ec75543b62059f1e403719dab59c24cb8b6c
elijahgott <gottelijah@gmail.com>
2026-03-11 20:42:25 -0500

    renders picture of me to triangle, can't render a quad without crashing

--- src/Main.java
+++ src/Main.java
@@ -4,14 +4,13 @@ import org.lwjgl.glfw.*;
 import org.lwjgl.opengl.*;
 
 import java.awt.*;
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
+import java.nio.ByteBuffer;
 import java.nio.IntBuffer;
-import java.nio.file.Files;
 
+import static org.lwjgl.stb.STBImage.*;
 import static org.lwjgl.glfw.GLFW.*;
 import static org.lwjgl.opengl.ARBVertexArrayObject.*;
+
 import static org.lwjgl.opengl.GL11.*;
 import static org.lwjgl.opengl.GL15C.GL_ARRAY_BUFFER;
 import static org.lwjgl.opengl.GL15C.GL_STATIC_DRAW;
@@ -19,6 +18,7 @@ import static org.lwjgl.opengl.GL15C.glBindBuffer;
 import static org.lwjgl.opengl.GL15C.glBufferData;
 import static org.lwjgl.opengl.GL15C.glGenBuffers;
 import static org.lwjgl.opengl.GL20.*;
+import static org.lwjgl.opengl.GL30.glGenerateMipmap;
 import static org.lwjgl.system.MemoryUtil.*;
 
 public class Main {
@@ -38,36 +38,55 @@ public class Main {
 
     int vao;
     int vbo;
+    int ebo;
 
     private static final float[] vertices = {
-            // positions         // colors
-            0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,  // bottom right
-            -0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,  // bottom left
-            0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f   // top
+            // positions          // colors           // texture coords
+            0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f, // top right
+            0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f, // bottom right
+            -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f, // bottom left
+            -0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f  // top left
+    };
+
+    private static final int[] indices = {
+            0, 1, 3, // first triangle
+            1, 2, 3 // second triangle
     };
 
     // vertex shader
     String vertexShaderSource = "#version 330 core\n" +
             "layout (location = 0) in vec3 aPos;\n" +
             "layout (location = 1) in vec3 aColor;\n" +
+            "layout (location = 2) in vec2 aTexCoord;\n" +
             "\n" +
             "out vec3 ourColor;\n" +
+            "out vec2 texCoord;\n" +
             "\n" +
             "void main()\n" +
             "{\n" +
             "    gl_Position = vec4(aPos, 1.0F);\n" +
             "    ourColor = aColor;\n" +
-            "}\0";
+            "    texCoord = vec2(aTexCoord.x, aTexCoord.y);\n" +
+            "}";
 
     // fragment shader
     String fragmentShaderSource = "#version 330 core\n" +
             "out vec4 FragColor;\n" +
+            "\n" +
             "in vec3 ourColor;\n" +
+            "in vec2 texCoord;\n" +
+            "\n" +
+            "// texture sampler\n" +
+            "uniform sampler2D texture1;\n" +
             "\n" +
             "void main()\n" +
             "{\n" +
-            "    FragColor = vec4(ourColor, 1.0F);\n" +
-            "}\0";
+            "    FragColor = texture(texture1, texCoord);\n" +
+            "}";
+
+    // load texture
+    int texture1;
+    int texture2;
 
     public boolean running = false;
     public Timer timer = new Timer();
@@ -98,9 +117,14 @@ public class Main {
 
         // RENDER LOOP
         while(!glfwWindowShouldClose(window)){
+            glfwPollEvents();
+
             // clear screen so it can be redrawn
             glClearColor(red, green, blue, 1F);
             glClear(GL_COLOR_BUFFER_BIT);
+
+            // bind texture
+            glBindTexture(GL_TEXTURE_2D, texture1);
 
             // use shader program
             glUseProgram(shaderProgram);
@@ -115,13 +139,13 @@ public class Main {
             // render triangle
             glBindVertexArray(vao);
             glDrawArrays(GL_TRIANGLES, 0, 3);
+//            glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0); // this crashes my program??
 
             // swap buffers
             // double buffers, one is shown on screen and one is the next frame
             glfwSwapBuffers(window);
             glfwSwapInterval(1);
 
-            glfwPollEvents();
 
             delta = timer.getDelta();
 
@@ -184,14 +208,19 @@ public class Main {
 
 //        System.out.println("OpenGL Version: " + glGetString(GL_VERSION));
 
+        // TEXTURES !!
+        texture1 = loadTexture("res/textures/elo.jpg");
+
         // SHADERS !!!
 
         // vertex array object
         vao = glGenVertexArrays();
         // vertex buffer object
         vbo = glGenBuffers();
+        // ebo??
+        ebo = glGenBuffers();
 
-        setUpVertexData(vao, vbo);
+        setUpVertexData(vao, vbo, ebo);
 
         // VERTEX SHADER
         final int vertexShader = createShader(GL_VERTEX_SHADER, vertexShaderSource);
@@ -222,8 +251,10 @@ public class Main {
     }
 
     public void dispose(){
-        glDeleteVertexArrays(1);
-        glDeleteBuffers(1);
+        glDeleteVertexArrays(vao);
+        glDeleteBuffers(vbo);
+        glDeleteBuffers(ebo);
+        glDeleteTextures(texture1);
         glDeleteProgram(shaderProgram);
 
         // after window is terminated
@@ -318,7 +349,7 @@ public class Main {
         return shader;
     }
 
-    private static void setUpVertexData(int vao, int vbo) {
+    private static void setUpVertexData(int vao, int vbo, int ebo) {
         // Bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
         glBindVertexArray(vao);
 
@@ -326,12 +357,19 @@ public class Main {
         glBufferData(GL_ARRAY_BUFFER, vertices, GL_STATIC_DRAW);
 
         // position attribute
-        glVertexAttribPointer(0, 3, GL_FLOAT, false, 6 * Float.BYTES, NULL);
+        glVertexAttribPointer(0, 3, GL_FLOAT, false, 8 * Float.BYTES, NULL);
         glEnableVertexAttribArray(0);
 
         // color attribute
-        glVertexAttribPointer(1, 3, GL_FLOAT, false, 6 * Float.BYTES, 3 * Float.BYTES);
+        glVertexAttribPointer(1, 3, GL_FLOAT, false, 8 * Float.BYTES, 3 * Float.BYTES);
         glEnableVertexAttribArray(1);
+
+        // texture attribute
+        glVertexAttribPointer(2, 2, GL_FLOAT, false, 8 * Float.BYTES, 6 * Float.BYTES);
+        glEnableVertexAttribArray(2);
+
+        glBindBuffer(GL_ARRAY_BUFFER, ebo);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices, GL_STATIC_DRAW);
 
         // note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
         glBindBuffer(GL_ARRAY_BUFFER, 0);
@@ -339,6 +377,42 @@ public class Main {
         // You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
         // VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
         glBindVertexArray(0);
+    }
+
+    private static int loadTexture(String path){
+        final int texture = glGenTextures();
+
+        glBindTexture(GL_TEXTURE_2D, texture);
+
+        // set texture wrapping parameters
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+        // set texture filtering parameters
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+        // load image
+        try(MemoryStack stack = MemoryStack.stackPush()) {
+            IntBuffer width = stack.ints(0);
+            IntBuffer height = stack.ints(0);
+            IntBuffer nrChannels = stack.ints(0);
+
+            stbi_set_flip_vertically_on_load(true);
+            ByteBuffer data = stbi_load(path, width, height, nrChannels, 0);
+
+            if(data != null){
+                glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width.get(0), height.get(0), 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+                glGenerateMipmap(GL_TEXTURE_2D);
+            }
+            else{
+                System.err.println("Could not load texture! " + path);
+            }
+
+            stbi_image_free(data);
+        }
+
+        return texture;
     }
 
     public static void main(String[] args) {
